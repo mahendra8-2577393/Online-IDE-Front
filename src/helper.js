9d4df8a3d660cf7a15c0f2ba209496aa4aa57008
Mahendra2577393 <mahendrannu8824@gmail.com>
2024-10-22 02:35:08 +0530

    deployLinkAdd

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
-export const toggleClass = (el,className) => {
+export const toggleClass = (el, className) => {
   let elem = document.querySelector(el);
   elem.classList.toggle(className);
 };
 
-export const removeClass = (el,className) => {
+export const removeClass = (el, className) => {
   let elem = document.querySelector(el);
   elem.classList.remove(className);
 };
 
-
-export const api_base_url = "http://localhost:5000"
+export const api_base_url = "https://online-ide-backend-tzro.onrender.com";
