d1812e64620296d3418621bdad6e24b7c5f5b0e3
Mahendra2577393 <mahendrannu8824@gmail.com>
2024-10-24 15:52:05 +0530

    changesInHome

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -42,81 +42,72 @@ const SignUp = () => {
 
   return (
     <>
-      <div className="container w-screen min-h-screen flex items-center justify-center relative">
-        {/* Background Image */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: `url(${image})`, backgroundSize: "cover" }}
-        ></div>
-
-        {/* Dark Overlay for contrast (optional) */}
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-
-        {/* Form Container */}
-        <div className="relative z-10 w-[300px] lg:w-[35%] flex flex-col items-center lg:items-start justify-center py-12 px-6 bg-white bg-opacity-90 rounded-3xl shadow-md">
-          <img
-            className="w-[200px] sm:w-[250px] lg:w-[300px] mb-4"
-            src={logo}
-            alt="Logo"
-          />
-          <form onSubmit={submitForm} className="w-full mt-[30px]">
-            <div className="inputBox mb-4">
+      <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
+        <div className="left w-[35%]">
+          <img className="w-[300px]" src={logo} alt="" />
+          <form onSubmit={submitForm} className="w-full mt-[60px]" action="">
+            <div className="inputBox">
               <input
                 required
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
                 value={username}
                 type="text"
                 placeholder="Username"
-                className="w-full p-2 border border-gray-300 rounded-xl"
               />
             </div>
 
-            <div className="inputBox mb-4">
+            <div className="inputBox">
               <input
                 required
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
                 value={name}
                 type="text"
                 placeholder="Name"
-                className="w-full p-2 border border-gray-300 rounded-xl"
               />
             </div>
 
-            <div className="inputBox mb-4">
+            <div className="inputBox">
               <input
                 required
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
                 value={email}
                 type="email"
                 placeholder="Email"
-                className="w-full p-2 border border-gray-300 rounded-xl"
               />
             </div>
 
-            <div className="inputBox mb-4">
+            <div className="inputBox">
               <input
                 required
-                onChange={(e) => setPwd(e.target.value)}
+                onChange={(e) => {
+                  setPwd(e.target.value);
+                }}
                 value={pwd}
                 type="password"
                 placeholder="Password"
-                className="w-full p-2 border border-gray-300 rounded-xl"
               />
             </div>
 
-            <p className="text-blue-900 font-extrabold mb-2">
-              Already have an account?{" "}
+            <p className="text-[gray]">
+              Already have an account{" "}
               <Link to="/login" className="text-[#00AEEF]">
-                Login
+                login
               </Link>
             </p>
 
             <p className="text-red-500 text-[14px] my-2">{error}</p>
 
-            <button className="btnBlue w-full mt-[20px] rounded-3xl">
-              Sign Up
-            </button>
+            <button className="btnBlue w-full mt-[20px]">Sign Up</button>
           </form>
+        </div>
+        <div className="right w-[55%]">
+          <img className="h-[100vh] w-[100%] object-cover" src={image} alt="" />
         </div>
       </div>
     </>
